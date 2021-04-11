a1bd847d206f8060c90920f6f91aba0b71360c00
lhthang1998 <lhthang.1998@gmail.com>
2021-04-11 08:17:43 +0700

    fix layout about

--- lib/about/About.dart
+++ lib/about/About.dart
@@ -1,6 +1,7 @@
 import 'package:auto_animated/auto_animated.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:my_portfolio/OrientationSwitcher.dart';
 import 'package:my_portfolio/utils/constant.dart';
 import 'package:my_portfolio/utils/functions.dart';
 
@@ -31,6 +32,7 @@ class _AboutState extends State<About> {
       ));
     }
     return new Column(
+      mainAxisSize: MainAxisSize.min,
       crossAxisAlignment: CrossAxisAlignment.start,
       children: _list,
     );
@@ -38,96 +40,102 @@ class _AboutState extends State<About> {
 
   @override
   Widget build(BuildContext context) {
-    final width = MediaQuery.of(context).size.width *
-        MediaQuery.of(context).devicePixelRatio;
-    final height = MediaQuery.of(context).size.height *
-        MediaQuery.of(context).devicePixelRatio;
+    final pixelRatio = MediaQuery.of(context).devicePixelRatio;
+    final width = MediaQuery.of(context).size.width * pixelRatio;
+    final height = MediaQuery.of(context).size.height * pixelRatio;
+    bool isPortrait =
+        MediaQuery.of(context).orientation == Orientation.portrait;
     return Container(
-      width: width,
-      child: Wrap(
-        direction: Axis.horizontal,
-        alignment: width / MediaQuery.of(context).devicePixelRatio < 1000
-            ? WrapAlignment.center
-            : WrapAlignment.start,
-        crossAxisAlignment: WrapCrossAlignment.start,
-        children: [
-          Container(
-              padding: EdgeInsets.only(top: 50, bottom: 50),
-              constraints: BoxConstraints(maxWidth: 300, maxHeight: 400),
-              child: Center(
-                child: Image(
-                  width: width / 5,
-                  fit: BoxFit.contain,
-                  image: AssetImage("images/avt.jpeg"),
-                ),
-              )),
-          Container(
-            padding: EdgeInsets.only(top: 50),
-            child: AnimateIfVisible(
-              key: Key("about"),
-              duration: Duration(seconds: 1),
-              builder: (context, animation) {
-                return FadeTransition(
-                  opacity: Tween<double>(
-                    begin: 0,
-                    end: 1,
-                  ).animate(animation),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        "ABOUT ME",
-                        style: GoogleFonts.montserrat(fontSize: 25),
-                      ),
-                      Text("Software Engineer",
-                          style: GoogleFonts.merriweather(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 50,
-                          )),
-                      _renderDescription(),
-                      SizedBox(
-                        height: 10,
-                      ),
-                      RaisedButton(
-                        onPressed: () {
-                          downloadFile(pdfCV);
-                        },
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(80.0)),
-                        padding: const EdgeInsets.all(0.0),
-                        child: Ink(
-                          decoration: const BoxDecoration(
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(80.0)),
-                              gradient: LinearGradient(
-                                colors: [
-                                  Colors.cyan,
-                                  Colors.indigo,
-                                ],
-                              )),
-                          child: Container(
-                            width: 150,
-                            height: 50, // min sizes for Material buttons
-                            alignment: Alignment.center,
-                            child: Text(
-                              'Download CV',
-                              textAlign: TextAlign.center,
+        constraints: BoxConstraints(maxWidth: width, maxHeight: height),
+        child: OrientationSwitcher(
+          children: [
+            Container(
+                constraints: BoxConstraints(maxWidth: 600, maxHeight: 800),
+                child: Center(
+                  child: Image(
+                    width: width / 5,
+                    fit: BoxFit.contain,
+                    image: AssetImage("images/avt.jpeg"),
+                  ),
+                )),
+            Flexible(
+              fit: FlexFit.loose,
+              child: Container(
+                child: AnimateIfVisible(
+                  key: Key("about"),
+                  duration: Duration(seconds: 1),
+                  builder: (context, animation) {
+                    return FadeTransition(
+                      opacity: Tween<double>(
+                        begin: 0,
+                        end: 1,
+                      ).animate(animation),
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        mainAxisAlignment: isPortrait
+                            ? MainAxisAlignment.start
+                            : MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "ABOUT ME",
+                            style: GoogleFonts.montserrat(fontSize: 25),
+                          ),
+                          Text("Software Engineer",
                               style: GoogleFonts.merriweather(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 15,
-                                  color: Colors.white),
+                                fontWeight: FontWeight.bold,
+                                fontSize: 50,
+                              )),
+                          // GridView.count(
+                          //   crossAxisCount: 2,
+                          //   children: [
+
+                          //   ],
+                          // ),
+                          _renderDescription(),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          RaisedButton(
+                            onPressed: () {
+                              downloadFile(pdfCV);
+                            },
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(80.0)),
+                            padding: const EdgeInsets.all(0.0),
+                            child: Ink(
+                              decoration: const BoxDecoration(
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(80.0)),
+                                  gradient: LinearGradient(
+                                    colors: [
+                                      Colors.cyan,
+                                      Colors.indigo,
+                                    ],
+                                  )),
+                              child: Container(
+                                width: 150,
+                                height: 50, // min sizes for Material buttons
+                                alignment: Alignment.center,
+                                child: Text(
+                                  'Download CV',
+                                  textAlign: TextAlign.center,
+                                  style: GoogleFonts.merriweather(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 15,
+                                      color: Colors.white),
+                                ),
+                              ),
                             ),
                           ),
-                        ),
+                        ],
                       ),
-                    ],
-                  ),
-                );
-              },
+                    );
+                  },
+                ),
+              ),
             ),
-          ),
-        ],
-      ),
-    );
+          ],
+        ));
   }
 }
