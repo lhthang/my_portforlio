7ae94566234ad39a823fc6d9d0eed97f87d71b2e
lhthang1998 <lhthang.1998@gmail.com>
2021-04-24 18:22:06 +0700

    Built web

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -18,8 +18,8 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "691777b51d364ca62f0c0167981ba3f1",
-"/": "691777b51d364ca62f0c0167981ba3f1",
+"index.html": "27847fa82ad67b75d3893927ec3d2fc6",
+"/": "27847fa82ad67b75d3893927ec3d2fc6",
 "main.dart.js": "030666e43936f79056ffb15daedd1c11",
 "manifest.json": "7268d731e85213fad6b35f4c3b52f2e6",
 "version.json": "aaafa928a4f8616604eca12d830fe765"
